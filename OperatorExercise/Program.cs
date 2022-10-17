0bf123630d232cbdf691bea12b28f4e1a28e640d
David Figone <david.louis.figone@gmail.com>
2022-10-17 08:52:22 -0500

    Final Commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
             int a = 17;
@@ -11,7 +15,17 @@
             if (a == 17 && b == 4)
             {
                 Console.WriteLine($"{a}/{b} is {b} remainder {remainder}");
-            } 
+
+                Console.WriteLine("What is the radius of your circle?");
+
+
+                Console.Write("Enter Radius: ");
+                double Radius = Convert.ToDouble(Console.ReadLine());
+                double Area = Math.PI * Radius * Radius;
+                Console.WriteLine("Area of circle: " + Area);
+                Console.ReadKey();
+
+            }
         }
     }
 }
